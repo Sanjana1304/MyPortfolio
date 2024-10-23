72c66e4e5522c7016ccbca164309c6bc4a69c0a3
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-23 18:59:57 +0530

    props added

--- frontend/src/components/ExpBox.jsx
+++ frontend/src/components/ExpBox.jsx
@@ -1,28 +1,12 @@
 import React from 'react'
 
-const ExpBox = ({}) => {
+const ExpBox = ({role,org,desc}) => {
   return (
     <div className='w-3/4 p-5 py-7 gradient-anim-4 rounded'>
-        <h1 className='font-semibold text-xl'>MERN Stack Engineer Intern</h1>
-        <i>Victory Group Of Institutions</i>
+        <h1 className='font-semibold text-xl'>{role}</h1>
+        <i>{org}</i>
         <ul className='text-sm list-disc list-inside text-gray-300'>
-            <li>
-            Spearheading the development of a scalable, feature-rich web application for a prominent educational
-            institution, delivering a seamless digital experience for both online and offline courses.
-
-            </li>
-            <li>
-            Increased student enrollment by 20% by architecting a responsive landing page using React and
-            TailwindCSS.
-            </li>
-            <li>
-            Developed a secure enrollment system that streamlined the onboarding process, reducing manual errors by
-            30% and improving administrative efficiency.
-            </li>
-            <li>
-            Collaborated with cross-functional teams to continuously optimize features, resulting in a 15% increase in
-            user engagement.
-            </li>
+            {desc && desc.map((d,index) => <li key={index}>{d}</li>)}
         </ul>
 
     </div>
--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
         <h1 className='text-4xl mb-6 font-bold px-5'>Working Experiences</h1>
         <div className='flex justify-center'>
             <div className=' w-[88%]'>
-                <ExpBox role={'MERN Stack Engineer Intern'}/>
+                <ExpBox role={'MERN Stack Engineer Intern'} org={'Victory Group Of Institutions'} desc={['Spearheading the development of a scalable, feature-rich web application for a prominent educational institution, delivering a seamless digital experience for both online and offline courses.','Increased student enrollment by 20% by architecting a responsive landing page using React and TailwindCSS.','Developed a secure enrollment system that streamlined the onboarding process, reducing manual errors by 30% and improving administrative efficiency.','Collaborated with cross-functional teams to continuously optimize features, resulting in a 15% increase in user engagement.']}/>
             </div>
             
         </div>
