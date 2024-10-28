f6c5b0399d9997e6b9ba71cb415e0ce962ecb3b6
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-28 15:46:15 +0530

    switch case added for projects desc

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -9,10 +9,39 @@ const Projects = () => {
         <div className="flex flex-col xl:flex-row justify-around">
             <Carousel onHover={setHoveredItem}/>
             <div className='shadow-lg w-[85%] mx-auto xl:w-[44%] p-5 py-10 flex justify-center items-center'>
-                {hoveredItem ? (
+                {/* {hoveredItem ? (
+                    
                     <p>Hovered Item: {hoveredItem}</p>
                 ) : (
                     <p className='font-bold'>HOLD AND MOVE THE MOUSE TO ROTATE THE CAROUSEL AND HOVER OVER AN ITEM IN THE CAROUSEL TO SEE THE PROJECT DESCRIPTION.</p>
+                )} */}
+                {hoveredItem ? (
+                    <p>
+                        {hoveredItem === 1
+                            ? 'Hello'
+                            : hoveredItem === 2
+                            ? 'Description for item 2'
+                            : hoveredItem === 3
+                            ? 'Description for item 3'
+                            : hoveredItem === 4
+                            ? 'Description for item 4'
+                            : hoveredItem === 5
+                            ? 'Description for item 5'
+                            : hoveredItem === 6
+                            ? 'Description for item 6'
+                            : hoveredItem === 7
+                            ? 'Description for item 7'
+                            : hoveredItem === 8
+                            ? 'Description for item 8'
+                            : hoveredItem === 9
+                            ? 'Description for item 9'
+                            : 'Default description'
+                        }
+                    </p>
+                ) : (
+                    <p className='font-bold'>
+                        HOLD AND MOVE THE MOUSE TO ROTATE THE CAROUSEL AND HOVER OVER AN ITEM IN THE CAROUSEL TO SEE THE PROJECT DESCRIPTION.
+                    </p>
                 )}
             </div>
         </div>
