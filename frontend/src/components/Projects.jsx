4f03344b370c56276bb0f8750a560a3a559c77c7
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-28 22:07:51 +0530

    adjusted project margin

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
     const [hoveredItem, setHoveredItem] = useState(null);
   
     return (
-    <div id='projects' className=' mt-2 p-3 gradient-anim-3 rounded-lg '>
-        <h1 className='text-4xl mb-6 font-bold px-5'>Projects</h1>
+    <div id='projects' className='mt-2 p-3 gradient-anim-3 rounded-lg '>
+        <h1 className='text-4xl md:mb-6 font-bold px-5'>Projects</h1>
         <div className="flex flex-col xl:flex-row justify-around">
             <Carousel onHover={setHoveredItem}/>
             <div className='shadow-lg rounded-lg w-[85%] mx-auto xl:w-[44%] p-5 py-10 flex justify-center items-center gradient-anim-2'>
