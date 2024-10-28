52f15a241a1ccb9ab7f23cc29e8d2176cada77db
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-28 21:54:27 +0530

    added more to explore content

--- frontend/src/components/More.jsx
+++ frontend/src/components/More.jsx
@@ -6,16 +6,19 @@ const More = () => {
 
     const moreData = [
         {
-            feedback: 'I am veryyyyy',
-            name: 'John Doe',
+            heading: 'Woohoo! Got My IBM Certification',
+            datee: 'Sep 2024',
+            content: 'Excited to share that I have completed the Developing Frontend Apps with React course as part of the IBM Full Stack Software Developer Professional Certificate program. During this course, I gained valuable skills in building dynamic web applications using React, including: 1. Mastering function components, 2. Leveraging hooks like useState and useEffect for efficient state and side-effect management, 3. Understanding advanced concepts like Redux for global state management, 4. Completing a final project by building a React shopping cart app using a combination of components, actions, and reducers',
         },
         {
-            feedback: 'I am very',
-            name: 'John Doe',
+            heading: 'My First Open Source Contribution',
+            datee: 'Aug 2024',
+            content: 'Excited to Share My Latest Open Source Contribution! I had the incredible opportunity to contribute to GrapplTech platform, Grabbit & Go, by designing and developing a stunning hero section component. Using React and Bootstrap, I created a responsive and visually appealing hero section that effectively captures user attention across all device views—desktop, mobile, and tablet. This component is now part of a curated selection of reusable web components, helping developers build websites more efficiently and collaboratively.',
         },
         {
-            feedback: 'I am very',
-            name: 'John Doe',
+            heading: 'Gold Badge in Problem Solving on HackerRank',
+            datee: 'Aug 2023',
+            content: 'Thrilled to share that I have earned the Gold Badge in Problem Solving on HackerRank! This journey has been a rewarding challenge, helping me sharpen my analytical and coding skills. I am excited to apply these skills in real-world projects and keep pushing my boundaries. Here is to continuous learning and growth! ',
         },
 
     ];
@@ -26,15 +29,16 @@ const More = () => {
   return (
 
     <section id='more' className='rounded-lg p-3 mt-3 bg-[#ffd2df]'>
-    <h1 className='font-bold text-3xl text-center'>More To Explore</h1>
+    <h1 className='font-bold text-3xl text-center'>More About Me</h1>
     <div className="testimonial-container relative overflow-hidden w-full flex flex-col justify-center items-center align-items-center">
         {moreData.length>0 && moreData.map((testimonial, index) => (
             <div
                 key={index}
                 className={`testimonial ${index === currentIndex ? 'block' : 'hidden'}`}
             >
-                <p className="text-center mb-5">{`"${testimonial.feedback}"`}</p>
-                <p className="text-center">{` - ${testimonial.name} (Enrolled in ${testimonial.courseName})`}</p>
+                <h1 className="mt-3 text-xl font-semibold text-center mb-2">{`${testimonial.heading}`}</h1>
+                <p className="mt-1 text-gray-800 font-semibold text-center mb-2">{`${testimonial.datee}`}</p>
+                <p className="text-gray-600 text-center mx-auto w-[75%] mb-3"><i>{`"${testimonial.content}"`}</i></p>
             </div>
         ))}
     </div>
