23c39215127cb1fb9f27a146cdae226522b01a2d
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-28 16:07:52 +0530

    proj desc added

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -16,32 +16,131 @@ const Projects = () => {
                             ?   <>
                                     <h1 className='font-bold text-xl'>BookAStay - Hotel Booking App</h1>
                                     <p className='text-sm'>BookAStay is a hotel booking application built with the MERN stack, allowing users to book hotels based on preferences like price, amenities, and hotel type. It provides a user dashboard to track stays, upcoming bookings, and past reservations, while hotel owners get insights through an analytics dashboard to monitor revenue and booking rates. The app features a sleek design using TailwindCSS for a smooth, modern user experience.</p>
-                                    <div className="flex">
-                                        <button>
+                                    <div className="flex mt-5">
+                                        <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
                                             View Code
                                         </button>
                                         
-                                        <button>
+                                        <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
                                             View App
                                         </button>
                                     </div>
                                 </>
                             : hoveredItem === 2
-                            ? 'Description for item 2'
+                            ? <>
+                            <h1 className='font-bold text-xl'>MediMatch - AI Powered Patient Case Similarity Match</h1>
+                            <p className='text-sm'>MediMatch is a AI powered web application that provides users with patient case similarity. Our platform uses state-of-the-art machine learning algorithms to analyze patient data and find similar cases. By leveraging advanced technology, we help medical professionals make more informed decisions and improve patient outcomes.</p>
+                            <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 3
-                            ? 'Description for item 3'
+                            ? <>
+                            <h1 className='font-bold text-xl'>Netflix Clone</h1>
+                            <p className='text-sm'>This Netflix homepage clone replicates the look and feel of the official Netflix landing page using HTML, CSS, and JavaScript. It includes a hero section with a featured movie banner, navigation bar, and a row of popular shows and movies. The design focuses on simplicity and responsiveness, capturing the essence of Netflix's user interface.</p>
+                            <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 4
-                            ? 'Description for item 4'
+                            ? <>
+                            <h1 className='font-bold text-xl'>IDEA</h1>
+                            <p className='text-sm'>Introducing IDEA - Intelligent Digital Educational Assist: a Python app with Tkinter GUI. It's inclusive for all, providing a voice assist for the visually impaired. Users interact with modules, increasing engagement. Features include activity tracking, history, a library for saving lectures, and a recommendation system based on past views.</p>
+                           <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 5
-                            ? 'Description for item 5'
+                            ? <>
+                            <h1 className='font-bold text-xl'>Movie Ticket Booking System</h1>
+                            <p className='text-sm'>This movie booking application enables users to browse movies and theaters across various cities and book tickets seamlessly. It provides real-time information on showtimes, theater locations, and seat availability, making the booking process smooth and hassle-free. With an intuitive interface, users can quickly select preferred showtimes and seats for an enhanced movie-going experience.</p>
+                           <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 6
-                            ? 'Description for item 6'
+                            ? 
+                            <>
+                            <h1 className='font-bold text-xl'>Sudoku Solver App</h1>
+                            <p className='text-sm'>This Sudoku game offers users two engaging options: they can play a new Sudoku puzzle or input a puzzle of their own and receive an instant solution. With an easy-to-use interface, players can enjoy solving puzzles directly or quickly check solutions for any provided puzzle, enhancing their gameplay and learning experience.</p>
+                           <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 7
-                            ? 'Description for item 7'
+                            ? 
+                            <>
+                            <h1 className='font-bold text-xl'>Recipe Finder & Meal Planner</h1>
+                            <p className='text-sm'>A react based recipe finder app which helps users to browse for recipes based on ingredients, dishes and save them for later use. The app also helps users to get a curated meal plan based on their target calories and diet type.</p>
+                           <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 8
-                            ? 'Description for item 8'
+                            ? 
+                            <>
+                            <h1 className='font-bold text-xl'>Amazon Clone</h1>
+                            <p className='text-sm'>This Amazon clone replicates the core layout and design of the Amazon homepage using HTML, CSS, and JavaScript. It includes a navigation bar with categories, a product display section, and a search bar to simulate browsing for products. The design focuses on delivering a familiar shopping experience, complete with interactive elements and a responsive layout to mimic Amazon’s user-friendly interface.</p>
+                           <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : hoveredItem === 9
-                            ? 'Description for item 9'
+                            ? <>
+                            <h1 className='font-bold text-xl'>Resume Builder App</h1>
+                            <p className='text-sm'>This React-based Resume Builder app allows users to input their personal and professional details, such as education, skills, work experience, and contact information, to generate a polished resume. Users can choose from multiple customizable templates, ensuring a professional and tailored final product. With real-time previews and easy editing, this app streamlines the resume creation process, making it simple and efficient.</p>
+                           <div className="flex mt-5">
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View Code
+                                </button>
+                                
+                                <button className='mr-5 p-3 rounded-lg lg:py-2 px-4  bg-gradient-to-b from-pink-400 to-pink-700 hover:from-pink-700 hover:to-pink-400 text-white'>
+                                    View App
+                                </button>
+                            </div>
+                        </>
                             : 'Default description'
                         }
                     </p>
